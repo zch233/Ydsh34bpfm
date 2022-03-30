560d15698811b0bbf63cd4675897bd56faa9803c
zhangchao <529743595@qq.com>
2022-03-30 16:42:38 +0800

    feat: update Markdown style

--- src/layout/components/Markdown.vue
+++ src/layout/components/Markdown.vue
@@ -6,11 +6,22 @@
 
 <script>
 export default {
-    name: 'Markdown'
+    name: 'Markdown',
 }
 </script>
 
 <style lang="less">
 // https://theme.typora.io/
-@import "../../assets/styles/themes/enhanced/notion-light-enhanced.less";
+@import '../../assets/styles/themes/enhanced/notion-light-enhanced.less';
+
+.my-markdown {
+    max-width: 1200px;
+    margin: 0 auto;
+
+    img {
+        display: block;
+        max-width: 1000px;
+        margin: 10px 0;
+    }
+}
 </style>
